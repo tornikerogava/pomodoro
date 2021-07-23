731551c4887e4afd692ec3fa4eca15249cf08e5f
Tornike Rogava (laptop) <tokorogava@gmail.com>
2021-07-23 18:27:59 +0400

    code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    const [BreakLength, setBreakLength] = useState(5);
+    const [SessionLength, setSessionLength] = useState(25);
+
+
+    return (
+        <div className="App">
+            <div id="break-setup">
+                <div id="break-label">{BreakLength}</div>
+            </div>
+
+            <div id="session-setup">
+                <div id="session-label">{SessionLength}</div>
+            </div>
+        </div>
+    );
 }
+
 export default App;
