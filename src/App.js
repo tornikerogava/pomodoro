0f08c638693ec9a7210bf82fad83c9761ff4595b
Tornike Rogava (laptop) <tokorogava@gmail.com>
2021-07-24 16:27:17 +0400

    Added some functionality

--- src/App.js
+++ src/App.js
@@ -8,12 +8,22 @@ function App() {
 
     return (
         <div className="App">
-            <div id="break-setup">
-                <div id="break-label">{BreakLength}</div>
+            <div className="setup" id="break-setup">
+                <div id="break-label">Break Length</div>
+                <div id="break-length">{BreakLength}</div>
+                <div id="break-buttons" className="setup-buttons">
+                    <button id="break-increment" onClick={()=>setBreakLength(BreakLength+1)}>^</button>
+                    <button id="break-decrement" onClick={()=>setBreakLength(BreakLength-1)}>v</button>
+                </div>
             </div>
 
-            <div id="session-setup">
-                <div id="session-label">{SessionLength}</div>
+            <div className="setup" id="session-setup">
+                <div id="session-label">Session Length</div>
+                <div id="session-length">{SessionLength}</div>
+                <div id="session-buttons" className="setup-buttons">
+                    <button id="session-increment" onClick={()=>setSessionLength(SessionLength+1)}>^</button>
+                    <button id="session-decrement" onClick={()=>setSessionLength(SessionLength-1)}>v</button>
+                </div>
             </div>
         </div>
     );
